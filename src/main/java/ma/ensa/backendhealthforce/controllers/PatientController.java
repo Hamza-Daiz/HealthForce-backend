7756c140353ef09f850799cec04252a76fb72cb0
hamza <hitec.appdev@gmail.com>
2022-06-04 19:26:48 +0100

    add permition + delet id_patient

--- src/main/java/ma/ensa/backendhealthforce/controllers/PatientController.java
+++ src/main/java/ma/ensa/backendhealthforce/controllers/PatientController.java
@@ -37,52 +37,54 @@ public class PatientController {
   public void updateInfos(@RequestBody Patient upatient) {
     Optional<Patient> patient = patientRepository.findByEmail(upatient.getEmail());
 
-    if (!upatient.getAdress().equals(patient.get().getAdress())){
+    System.out.println(patient.get().getAdress());
+
+    if (patient.get().getAdress()==null || !upatient.getAdress().equals(patient.get().getAdress())){
       patient.get().setAdress(patient.get().getAdress());
     }
-    if (!upatient.getAssurance_medical().equals(patient.get().getAssurance_medical())){
+    if (patient.get().getAssurance_medical()==null || !upatient.getAssurance_medical().equals(patient.get().getAssurance_medical())){
       patient.get().setAssurance_medical(patient.get().getAssurance_medical());
     }
-    if (!upatient.getNum_assurance().equals(patient.get().getNum_assurance())){
+    if (patient.get().getNum_assurance()==null || !upatient.getNum_assurance().equals(patient.get().getNum_assurance())){
       patient.get().setNum_assurance(patient.get().getNum_assurance());
     }
-    if (!upatient.getBirth_date().equals(patient.get().getBirth_date())){
+    if (patient.get().getBirth_date()==null || !upatient.getBirth_date().equals(patient.get().getBirth_date())){
       patient.get().setBirth_date(patient.get().getBirth_date());
     }
-    if (!upatient.getCin().equals(patient.get().getCin())){
+    if (patient.get().getCin()==null || !upatient.getCin().equals(patient.get().getCin())){
       patient.get().setCin(patient.get().getCin());
     }
-    if (!upatient.getFirst_name().equals(patient.get().getFirst_name())){
+    if (patient.get().getFirst_name()==null || !upatient.getFirst_name().equals(patient.get().getFirst_name())){
       patient.get().setFirst_name(patient.get().getFirst_name());
     }
-    if (!upatient.getLast_name().equals(patient.get().getLast_name())){
+    if (patient.get().getLast_name()==null || !upatient.getLast_name().equals(patient.get().getLast_name())){
       patient.get().setLast_name(patient.get().getLast_name());
     }
-    if (!upatient.getGender().equals(patient.get().getGender())){
+    if (patient.get().getGender()==null || !upatient.getGender().equals(patient.get().getGender())){
       patient.get().setGender(patient.get().getGender());
     }
-    if (!upatient.getPhone().equals(patient.get().getPhone())){
+    if (patient.get().getPhone()==null || !upatient.getPhone().equals(patient.get().getPhone())){
       patient.get().setPhone(patient.get().getPhone());
     }
-    if (!upatient.getHome_phone().equals(patient.get().getHome_phone())){
+    if (patient.get().getHome_phone()==null || !upatient.getHome_phone().equals(patient.get().getHome_phone())){
       patient.get().setHome_phone(patient.get().getHome_phone());
     }
-    if (!upatient.getPoid().equals(patient.get().getPoid())){
+    if (patient.get().getPoid()==null || !upatient.getPoid().equals(patient.get().getPoid())){
       patient.get().setPoid(patient.get().getPoid());
     }
-    if (!upatient.getTaille().equals(patient.get().getTaille())){
+    if (patient.get().getTaille()==null || !upatient.getTaille().equals(patient.get().getTaille())){
       patient.get().setTaille(patient.get().getTaille());
     }
-    if (!upatient.getTemperature().equals(patient.get().getTemperature())){
+    if (patient.get().getTemperature()==null || !upatient.getTemperature().equals(patient.get().getTemperature())){
       patient.get().setTemperature(patient.get().getTemperature());
     }
-    if (!upatient.getGlecimie().equals(patient.get().getGlecimie())){
+    if (patient.get().getGlecimie()==null || !upatient.getGlecimie().equals(patient.get().getGlecimie())){
       patient.get().setGlecimie(patient.get().getGlecimie());
     }
-    if (!upatient.getDescription().equals(patient.get().getDescription())){
+    if (patient.get().getDescription()==null || !upatient.getDescription().equals(patient.get().getDescription())){
       patient.get().setDescription(patient.get().getDescription());
     }
-    if (!upatient.getTension().equals(patient.get().getTension())){
+    if (patient.get().getTension()==null || !upatient.getTension().equals(patient.get().getTension())){
       patient.get().setTension(patient.get().getTension());
     }
 
